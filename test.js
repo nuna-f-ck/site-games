d1fb652510d49da70b31645407f0b5a69e20c9a1
Maslova.M <mmaslova866@gmail.com>
2025-06-04 09:48:08 +0300

    try

--- test.js
+++ test.js
@@ -25,11 +25,11 @@ alert(a % b);
 
 let num = 1;
 num += 5;
-num = num - 3;
-num = num * 7;
-num = num / 3;
-num = num + 1;
-num = num - 1;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
 alert(num);
 
 let age = prompt('Сколько вам лет?')
